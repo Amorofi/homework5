236b1e19d49980e40dc71be6f50a22582a93bf04
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-11-06 20:38:23 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homework 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Hello, Davids' webpages</h1>
+
+      <a href = "page1.html">Page 1: </a>
+      <br>
+      <br>
+      <a href = "page2.html">Page 2: </a>
+      <br>
+      <br>
+      <a href = "page3.html">Page 3: </a>
+      <br>
+      <br>
+      <a href = "page4.html">Page 4: </a>
+      <br>
+      <br>
+      <a href = "page5.html">Page 5: </a>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
